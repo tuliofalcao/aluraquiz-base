1075fecfccae5fc2466063ef3a6147e7e58a4a77
Túlio Falcão <falcao.tulio@gmail.com>
2021-01-27 04:22:31 -0300

    Segundo dia da imersao

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,24 @@
-import styled from 'styled-components'
+/* eslint-disable no-unused-vars */
+/* eslint-disable func-names */
+/* eslint-disable no-console */
+/* eslint-disable react/jsx-no-comment-textnodes */
+import React from 'react';
+import styled from 'styled-components';
+
+import { useRouter } from 'next/router';
+import Head from 'next/head';
 import db from '../db.json';
 import Widget from '../src/components/Widget';
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
 
-//const BackgroundImage = styled.div`
+// const BackgroundImage = styled.div`
 //  background-image: url(${db.bg});
 //  flex: 1;
 //  background-size: cover;
 //  background-position: center;
-//`;
+// `;
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -24,28 +32,50 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
   return (
+    // eslint-disable-next-line react/jsx-filename-extension
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz - Modelo Base</title>
+      </Head>
       <QuizContainer>
-          <Widget>
-            <Widget.Header>
-                <h1>The Legend of Zelda</h1>
-            </Widget.Header>
-            <Widget.Content>
-              <p>lorem ipsum ist done fuck</p>
-            </Widget.Content>
-          </Widget>
-            
-          <Widget>
-            <Widget.Content>
-              <h1>Quizes da galera</h1>
+        <Widget>
+          <Widget.Header>
+            <h1>The Legend of Zelda</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  // name = infosDoEvento.target.value;
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Diz aí seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
+          </Widget.Content>
+        </Widget>
 
-              <p>lorem ipsum ist done fuck</p>
-            </Widget.Content>
-          </Widget>
-          <Footer />
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da galera</h1>
+
+            <p>lorem ipsum ist done fuck</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto"/>
+      <GitHubCorner projectUrl="https://github.com/omariosouto" />
     </QuizBackground>
-  )
+  );
 }
